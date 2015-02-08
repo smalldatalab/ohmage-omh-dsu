ccfa147844a8d1975a2c3bf8ebcf8b72782e9422
changun <changun.tw@gmail.com>
2015-02-08 17:04:54 -0500

    updating spring-security to 2.0.6 for non-expiring refresh token

--- authorization-server/src/main/java/org/openmhealth/dsu/configuration/OAuth2AuthorizationServerConfiguration.java
+++ authorization-server/src/main/java/org/openmhealth/dsu/configuration/OAuth2AuthorizationServerConfiguration.java
@@ -19,6 +19,7 @@ package org.openmhealth.dsu.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
@@ -45,11 +46,15 @@ public class OAuth2AuthorizationServerConfiguration extends AuthorizationServerC
     private AuthenticationManager authenticationManager;
 
     @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.withClientDetails(clientDetailsService);
+    }
+
+    @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 
         endpoints
                 .tokenStore(tokenStore)
-                .clientDetailsService(clientDetailsService)
                 .authenticationManager(authenticationManager);
     }
 }
