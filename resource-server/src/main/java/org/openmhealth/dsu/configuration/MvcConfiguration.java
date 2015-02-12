2e668736191f804475b5336a6fb09f26749d759e
changun <changun.tw@gmail.com>
2015-02-12 16:12:19 -0500

    properly serialize the OffsetDateTime

--- resource-server/src/main/java/org/openmhealth/dsu/configuration/MvcConfiguration.java
+++ resource-server/src/main/java/org/openmhealth/dsu/configuration/MvcConfiguration.java
@@ -2,8 +2,10 @@ package org.openmhealth.dsu.configuration;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.InstantDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.InstantSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -28,17 +30,23 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter {
         converters.add(converter());
 
     }
+
+    /**
+     * @return A new MappingJackson2HttpMessageConverter that will (de)serialize OffsetDateTime objects properly
+     */
     @Bean
     MappingJackson2HttpMessageConverter converter() {
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        SimpleModule module = new SimpleModule("OffsetDateTime").
-                                        addDeserializer(OffsetDateTime.class, InstantDeserializer.OFFSET_DATE_TIME);
+        SimpleModule module =
+                new SimpleModule("OffsetDateTime")
+                .addDeserializer(OffsetDateTime.class, InstantDeserializer.OFFSET_DATE_TIME)
+                .addSerializer(OffsetDateTime.class, InstantSerializer.OFFSET_DATE_TIME);
         converter.getObjectMapper()
                 .configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE, false)
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
                 .setPropertyNamingStrategy(
                         PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES)
-        .registerModule(module)
-                ;
+        .registerModule(module);
 
         return converter;
     }
