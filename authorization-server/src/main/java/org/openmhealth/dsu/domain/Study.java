4151da6c635ee69f01135d0f34ff316a8afbf71f
changun <changun.tw@gmail.com>
2015-03-25 21:40:22 -0400

    implement study enrollment page that enroll user via admin dashbaord db.

---
+++ authorization-server/src/main/java/org/openmhealth/dsu/domain/Study.java
@@ -0,0 +1,35 @@
+package org.openmhealth.dsu.domain;
+
+/**
+ * Created by Cheng-Kang Hsieh on 3/25/15.
+ */
+public class Study {
+    Long id;
+    String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public Study(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Study{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ authorization-server/src/main/java/org/openmhealth/dsu/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package org.openmhealth.dsu.exception;
+
+/**
+ * Created by Cheng-Kang Hsieh on 3/25/15.
+ */
+public class ResourceNotFoundException extends RuntimeException {
+}
