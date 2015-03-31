86bb7bbaf56d08421c9adac99b08e224c54a1dd1
changun <changun.tw@gmail.com>
2015-03-31 12:10:01 -0400

    show "Reconnect?" when user has already connected with the app beofre

--- authorization-server/src/main/java/org/openmhealth/dsu/controller/ShimProxyController.java
+++ authorization-server/src/main/java/org/openmhealth/dsu/controller/ShimProxyController.java
@@ -1,115 +1,75 @@
 package org.openmhealth.dsu.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClients;
+import org.openmhealth.dsu.service.ShimService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mobile.device.DeviceUtils;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.util.UrlUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.net.URLEncoder;
-import java.util.Objects;
+
 
 /**
+ * Controller for users to connect (i.e. authorize us to access their data) via a shim server.
  * Created by Cheng-Kang Hsieh on 3/5/15.
  */
 @Controller
 public class ShimProxyController {
     private static final Logger log = LoggerFactory.getLogger(ShimProxyController.class);
+    @Autowired
+    ShimService shimService;
 
-    private @Value("${internalShimServer.url}") String internalShimUrl;
-    private @Value("${application.url}") String rootUrl;
-    private @Autowired ObjectMapper mapper;
-
-
-    private String getCallbackUrl(String shim){
-        return UriComponentsBuilder.fromUriString(rootUrl)
-                .pathSegment("shims", "authorize", shim, "callback")
-                .build()
-                .toUriString();
-    }
-    private URI getInternalShimAuthorizeUrl(String shim, Authentication auth){
-        return UriComponentsBuilder.fromUriString(internalShimUrl)
-                .pathSegment("authorize", shim)
-                .queryParam("client_redirect_url", getCallbackUrl(shim))
-                .queryParam("username", auth.getName()).build().toUri();
-    }
-    private URI getInternalShimDeauthorizeUrl(String shim, Authentication auth){
-        return UriComponentsBuilder.fromUriString(internalShimUrl)
-                .pathSegment("de-authorize", shim)
-                .queryParam("username", auth.getName()).build().toUri();
-    }
-    private String captalize (String s){
-        return Character.toUpperCase(s.charAt(0)) + s.substring(1);
-    }
-
-
-    @RequestMapping(value="/shims/authorize/{shim}", method= RequestMethod.GET)
+    @RequestMapping(value = "/shims/authorize/{shim}", method = RequestMethod.GET)
     public String authorize(Authentication auth, @PathVariable("shim") String shim,
                             Model model,
                             HttpServletRequest servletRequest) throws IOException {
-
-
-
-        HttpClient client = HttpClients.createDefault();
-
-        // de-authorize the user first
-        HttpDelete shimDeauthorizeRequest = new HttpDelete(getInternalShimDeauthorizeUrl(shim, auth));
-        client.execute(shimDeauthorizeRequest).getStatusLine();
-
-        // get the authorization url
-        log.info(getInternalShimAuthorizeUrl(shim, auth).toString());
-        HttpGet shimRequest = new HttpGet(getInternalShimAuthorizeUrl(shim, auth));
-        shimRequest.setHeader("Accept", "application/json");
-
-        // extract authorization URL from the response
-        InputStream in = client.execute(shimRequest).getEntity().getContent();
-        String redirectUrl = mapper.readTree(in).get("authorizationUrl").textValue();
-
-        // check if it is a valid URL
-        if (UrlUtils.isAbsoluteUrl(redirectUrl)) {
-            // if the page is viewed on a mobile phone, use phone-specific uri instead
-            // FIXME, this should be done at the shim server.
-            if(DeviceUtils.getCurrentDevice(servletRequest).isMobile() && redirectUrl.startsWith("https://api.moves-app.com/oauth/v1/authorize")){
-                redirectUrl = redirectUrl.replace("https://api.moves-app.com/oauth/v1/authorize", "moves://app/authorize");
-
-            }
-            model.addAttribute("appName", captalize(shim));
-            model.addAttribute("url", redirectUrl);
-            return "connect";
-        }else{
-            throw new IOException("Shim returns invalid auth url" + redirectUrl);
+        Boolean connected;
+        try {
+            String redirectUrl = shimService.getAuthorizationUrl(shim, auth, servletRequest);
+            model.addAttribute("authorizationUrl", redirectUrl);
+            connected = false;
+        } catch (ShimService.UserIsAuthorizedException e) {
+            connected = true;
         }
+        model.addAttribute("appName", shim);
+        model.addAttribute("connected", connected);
+        return "connect";
     }
 
+    @RequestMapping(value = "/shims/reauthorize/{shim}", method = RequestMethod.GET)
+    public ModelAndView reauthorize(Authentication auth, @PathVariable("shim") String shim,
+                                    HttpServletRequest servletRequest) throws IOException {
 
-    @RequestMapping(value="/shims/authorize/{shim}/callback", method= RequestMethod.GET)
+
+        try {
+            shimService.deauthorize(shim, auth);
+            String redirectUrl = shimService.getAuthorizationUrl(shim, auth, servletRequest);
+            // immediate redirect to authorize url
+            return new ModelAndView("redirect:" + redirectUrl);
+        } catch (ShimService.UserIsAuthorizedException e) {
+            throw new RuntimeException("De-authorization failed");
+        }
+
+    }
+
+    @RequestMapping(value = "/shims/authorize/{shim}/callback", method = RequestMethod.GET)
     public String callback(Authentication auth,
                            @PathVariable("shim") String shim,
                            @RequestParam(required = false) String error,
                            Model model,
                            HttpServletRequest servletRequest) throws IOException {
-        if(error == null) {
-            model.addAttribute("appName", captalize(shim));
+        if (error == null) {
+            model.addAttribute("appName", shim);
             return "connected";
-        }else{
+        } else {
             return authorize(auth, shim, model, servletRequest);
         }
     }
---
+++ authorization-server/src/main/java/org/openmhealth/dsu/service/ShimServiceImpl.java
@@ -0,0 +1,111 @@
+package org.openmhealth.dsu.service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mobile.device.DeviceUtils;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
+/**
+ * Service that communicate with the shims server to get authorization url or remove the previous authorization.
+ * Created by Cheng-Kang Hsieh on 3/31/15.
+ */
+@Service
+public class ShimServiceImpl implements ShimService {
+    private static final Logger log = LoggerFactory.getLogger(ShimService.class);
+    private
+    @Value("${internalShimServer.url}")
+    String internalShimUrl;
+    private
+    @Value("${application.url}")
+    String rootUrl;
+    private
+    @Autowired
+    ObjectMapper mapper;
+
+
+    private String getCallbackUrl(String shim) {
+        return UriComponentsBuilder.fromUriString(rootUrl)
+                .pathSegment("shims", "authorize", shim, "callback")
+                .build()
+                .toUriString();
+    }
+
+    private URI getInternalShimAuthorizeUrl(String shim, Authentication auth) {
+        return UriComponentsBuilder.fromUriString(internalShimUrl)
+                .pathSegment("authorize", shim)
+                .queryParam("client_redirect_url", getCallbackUrl(shim))
+                .queryParam("username", auth.getName()).build().toUri();
+    }
+
+    private URI getInternalShimDeauthorizeUrl(String shim, Authentication auth) {
+        return UriComponentsBuilder.fromUriString(internalShimUrl)
+                .pathSegment("de-authorize", shim)
+                .queryParam("username", auth.getName()).build().toUri();
+    }
+
+    @Override
+    public String getAuthorizationUrl(String shim, Authentication auth, HttpServletRequest servletRequest) throws UserIsAuthorizedException, IOException {
+        HttpClient client = HttpClients.createDefault();
+        // get the authorization url
+        log.info(getInternalShimAuthorizeUrl(shim, auth).toString());
+        HttpGet shimRequest = new HttpGet(getInternalShimAuthorizeUrl(shim, auth));
+        shimRequest.setHeader("Accept", "application/json");
+
+        // extract authorization URL from the response
+        InputStream in = client.execute(shimRequest).getEntity().getContent();
+        JsonNode node = mapper.readTree(in);
+        if (node.has("isAuthorized") && node.get("isAuthorized").asBoolean()) {
+            log.info("User " + auth.getName() + " has connected with " + shim);
+            throw new UserIsAuthorizedException();
+        } else if (node.has("authorizationUrl")) {
+            // FIXME: What if Shim server fail
+            String redirectUrl = node.get("authorizationUrl").textValue();
+            // if the page is viewed on a mobile phone, use phone-specific uri instead
+            // FIXME, this should be done at the shim server.
+            if (DeviceUtils.getCurrentDevice(servletRequest).isMobile() && redirectUrl.startsWith("https://api.moves-app.com/oauth/v1/authorize")) {
+                redirectUrl = redirectUrl.replace("https://api.moves-app.com/oauth/v1/authorize", "moves://app/authorize");
+
+            }
+            return redirectUrl;
+        } else {
+            throw new RuntimeException("Unexpected response from shim server");
+        }
+    }
+
+    @Override
+    public boolean deauthorize(String shim, Authentication auth) throws IOException {
+        HttpClient client = HttpClients.createDefault();
+        // de-authorize the user first
+        HttpDelete shimDeauthorizeRequest = new HttpDelete(getInternalShimDeauthorizeUrl(shim, auth));
+        return client.execute(shimDeauthorizeRequest).getStatusLine().getStatusCode() == 200;
+    }
+
+    @Override
+    public boolean isAuthorized(String shim, Authentication auth) throws IOException {
+        HttpClient client = HttpClients.createDefault();
+        // get the authorization url
+        log.info(getInternalShimAuthorizeUrl(shim, auth).toString());
+        HttpGet shimRequest = new HttpGet(getInternalShimAuthorizeUrl(shim, auth));
+        shimRequest.setHeader("Accept", "application/json");
+
+        // extract authorization URL from the response
+        InputStream in = client.execute(shimRequest).getEntity().getContent();
+        JsonNode node = mapper.readTree(in);
+        return node.has("isAuthorized") && node.get("isAuthorized").asBoolean();
+    }
+}
