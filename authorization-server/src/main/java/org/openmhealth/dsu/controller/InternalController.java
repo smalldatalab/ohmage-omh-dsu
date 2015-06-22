3a61904a9b2d90ec87aa232f73dc2a5231df4912
changun <changun.tw@gmail.com>
2015-06-21 23:23:15 -0400

    put registerUser() back but makes it internal

--- authorization-server/src/main/java/org/openmhealth/dsu/controller/InternalController.java
+++ authorization-server/src/main/java/org/openmhealth/dsu/controller/InternalController.java
@@ -1,10 +1,12 @@
 package org.openmhealth.dsu.controller;
 
+import org.openmhealth.dsu.domain.EndUserRegistrationData;
 import org.openmhealth.dsu.domain.EndUserUserDetails;
 import org.openmhealth.dsu.service.EndUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -17,17 +19,27 @@ import org.springframework.security.oauth2.provider.*;
 import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
 import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.openmhealth.dsu.configuration.OAuth2Properties.END_USER_ROLE;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
- * Controller that is intended for the internal use only.
+ * This controller that is intended for the internal use only, MUST be accessed only by localhost.
  * Created by Cheng-Kang Hsieh on 4/4/15.
  */
 @Controller
@@ -37,14 +49,25 @@ public class InternalController {
     @Autowired
     AuthorizationServerTokenServices tokenService;
     @Autowired
-    UserDetailsService endUserService;
+    UserDetailsService userDetailService;
+    @Autowired
+    EndUserService endUserService;
+    @Autowired
+    private Validator validator;
 
+
+    /**
+     * Authorize the client (client_id) the access token to the given user (username)
+     * @param username username
+     * @param client_id client id
+     * @return access token in JSON
+     */
     @RequestMapping(value = "/internal/token", method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity<OAuth2AccessToken> createAccessToken( @RequestParam String username,  @RequestParam String client_id){
         OAuth2RequestFactory requestFactory = new DefaultOAuth2RequestFactory(clientService);
 
         clientService.loadClientByClientId(client_id);
-        UserDetails user = endUserService.loadUserByUsername(username);
+        UserDetails user = userDetailService.loadUserByUsername(username);
         HashMap<String, String> authorizationParameters = new HashMap<String, String>();
         authorizationParameters.put("username", username);
         authorizationParameters.put("client_id", client_id);
@@ -61,4 +84,42 @@ public class InternalController {
 
         return new ResponseEntity<>(accessToken, headers, HttpStatus.OK);
     }
+
+
+    /**
+     * Registers a new user that use username and password to sign in. Currently, we only allow system administrator
+     * to do so via backend.
+     *
+     * @param registrationData the registration data of the user
+     * @return a response entity with status OK if the user is registered, BAD_REQUEST if the request is invalid,
+     * or CONFLICT if the user exists
+     */
+    @RequestMapping(value = "/internal/users", method = POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> registerUser(@RequestBody EndUserRegistrationData registrationData) {
+
+        if (registrationData == null) {
+            return new ResponseEntity<>(BAD_REQUEST);
+        }
+
+        Set<ConstraintViolation<EndUserRegistrationData>> constraintViolations = validator.validate(registrationData);
+
+        if (!constraintViolations.isEmpty()) {
+            return new ResponseEntity<>(asErrorMessageList(constraintViolations), BAD_REQUEST);
+        }
+
+        if (endUserService.doesUserExist(registrationData.getUsername())) {
+            return new ResponseEntity<>(CONFLICT);
+        }
+
+        endUserService.registerUser(registrationData);
+
+        return new ResponseEntity<>(OK);
+    }
+
+    protected List<String> asErrorMessageList(Set<ConstraintViolation<EndUserRegistrationData>> constraintViolations) {
+
+        return constraintViolations.stream().map(ConstraintViolation::getMessage).collect(Collectors.toList());
+    }
+
+
 }
