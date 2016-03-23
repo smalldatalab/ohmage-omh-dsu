f2c8299e8e76c44db4cc0c3e21675e73e99ce657
Jared Sieling <jared.sieling@gmail.com>
2016-03-23 16:48:11 -0500

    Shim service can now fetch data or load from file.

--- dpu-server/src/main/groovy/org/openmhealth/dpu/processor/SyncFitbitProcessor.groovy
+++ dpu-server/src/main/groovy/org/openmhealth/dpu/processor/SyncFitbitProcessor.groovy
@@ -1,5 +1,7 @@
 package org.openmhealth.dpu.processor
 
+import groovy.json.JsonSlurper
+import org.openmhealth.dpu.service.OmhShimService
 import org.openmhealth.dpu.util.DataPoint
 import org.openmhealth.dpu.util.EndUser
 import org.slf4j.Logger
@@ -7,13 +9,38 @@ import org.slf4j.LoggerFactory
 import org.springframework.batch.item.ItemProcessor
 import org.springframework.stereotype.Component
 
+import javax.inject.Inject
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
 @Component("syncFitbitProcessor")
 public class SyncFitbitProcessor implements ItemProcessor<EndUser, List<DataPoint>> {
 
     private static final Logger log = LoggerFactory.getLogger(SyncFitbitProcessor.class);
 
+    @Inject
+    private OmhShimService omhShimService;
+
     @Override
     public List<DataPoint> process(EndUser user) throws Exception {
+
+        // Set date range for past calendar week, ending Sunday
+        LocalDate now = LocalDate.now()
+//        LocalDate now = LocalDate.parse('2016-02-03', DateTimeFormatter.ISO_LOCAL_DATE)
+        LocalDate endDate = now.minusDays(now.getDayOfWeek().getValue())
+        LocalDate startDate = endDate.minusDays(6)
+
+        // Fetch data
+        String responseString = omhShimService.getData(user, "fitbit", "activity", false, startDate, endDate);
+
+        JsonSlurper slurper = new JsonSlurper();
+        def root = slurper.parseText(responseString);
+
+        root.body?.each {
+            def item = it.result.content.summary
+        }
+
+
         List<DataPoint> dataPoints = new ArrayList<DataPoint>();
 
         DataPoint result = new DataPoint();
---
+++ dpu-server/src/main/groovy/org/openmhealth/dpu/service/OmhShimService.java
@@ -0,0 +1,82 @@
+package org.openmhealth.dpu.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.HttpClients;
+import org.openmhealth.dpu.util.EndUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.net.URI;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.stream.Collectors;
+
+/**
+ * (Description here)
+ *
+ * @author Jared Sieling.
+ */
+@Service
+public class OmhShimService {
+
+    private static final Logger log = LoggerFactory.getLogger(OmhShimService.class);
+
+    private
+    @Value("${omh.shim.fakedata}") Boolean useFakeData = false;
+
+    private
+    @Value("${omh.shim.url}") String shimUrl;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    public String getData(EndUser user, String shim, String endpoint, Boolean normalize, LocalDate startDate, LocalDate endDate) {
+        if (!useFakeData) {
+            HttpClient client = HttpClients.createDefault();
+            UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(shimUrl)
+                    .pathSegment("data", shim, endpoint)
+                    .queryParam("username", user.getUsername())
+                    .queryParam("normalize", normalize.toString())
+                    .queryParam("dateStart", startDate.format(DateTimeFormatter.ISO_LOCAL_DATE))
+                    .queryParam("dateEnd", endDate.format(DateTimeFormatter.ISO_LOCAL_DATE));
+
+            URI shimDataUrl = uriBuilder.build().toUri();
+
+            HttpGet shimRequest = new HttpGet(shimDataUrl);
+            shimRequest.setHeader("Accept", "application/json");
+
+            try {
+                HttpResponse response = client.execute(shimRequest);
+                String responseString = new BasicResponseHandler().handleResponse(response);
+
+                return responseString;
+            } catch (Exception ex) {
+                return null;
+            }
+
+        } else {
+            File file = new File("sample-data/omh-" + shim + "-" + endpoint + "-" + normalize.toString() + ".json");
+            try {
+                BufferedReader bR = new BufferedReader(  new FileReader(file));
+                String responseString = bR.lines().collect(Collectors.joining(""));
+                return responseString;
+            } catch (Exception ex) {
+                return null;
+            }
+        }
+    }
+
+
+
+}
