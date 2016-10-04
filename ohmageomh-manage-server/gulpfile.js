9f29dc3b1aa187a44e2b8923875abcfed1f11745
Jared Sieling <jared.sieling@gmail.com>
2016-10-04 10:56:33 -0500

    Add special handling for ui-grid fonts.

--- ohmageomh-manage-server/gulpfile.js
+++ ohmageomh-manage-server/gulpfile.js
@@ -34,32 +34,42 @@ gulp.task('clean', function () {
 });
 
 gulp.task('copy', function () {
-    return es.merge( 
+    return es.merge(
         gulp.src(config.bower + 'bootstrap/fonts/*.*')
-        .pipe(plumber({errorHandler: handleErrors}))
-        .pipe(changed(config.dist + 'content/fonts/'))
-        .pipe(rev())
-        .pipe(gulp.dest(config.dist + 'content/fonts/'))
-        .pipe(rev.manifest(config.revManifest, {
-            base: config.dist,
-            merge: true
-        }))
-        .pipe(gulp.dest(config.dist)),
+            .pipe(plumber({errorHandler: handleErrors}))
+            .pipe(changed(config.dist + 'content/fonts/'))
+            .pipe(rev())
+            .pipe(gulp.dest(config.dist + 'content/fonts/'))
+            .pipe(rev.manifest(config.revManifest, {
+                base: config.dist,
+                merge: true
+            }))
+            .pipe(gulp.dest(config.dist)),
+        gulp.src(config.bower + 'angular-ui-grid/**/*.{ttf,woff}')
+            .pipe(plumber({errorHandler: handleErrors}))
+            .pipe(changed(config.dist + 'content/fonts/'))
+            .pipe(rev())
+            .pipe(gulp.dest(config.dist + 'content/css/'))
+            .pipe(rev.manifest(config.revManifest, {
+                base: config.dist,
+                merge: true
+            }))
+            .pipe(gulp.dest(config.dist)),
         gulp.src(config.app + 'content/**/*.{woff,woff2,svg,ttf,eot,otf}')
-        .pipe(plumber({errorHandler: handleErrors}))
-        .pipe(changed(config.dist + 'content/fonts/'))
-        .pipe(flatten())
-        .pipe(rev())
-        .pipe(gulp.dest(config.dist + 'content/fonts/'))
-        .pipe(rev.manifest(config.revManifest, {
-            base: config.dist,
-            merge: true
-        }))
-        .pipe(gulp.dest(config.dist)),
+            .pipe(plumber({errorHandler: handleErrors}))
+            .pipe(changed(config.dist + 'content/fonts/'))
+            .pipe(flatten())
+            .pipe(rev())
+            .pipe(gulp.dest(config.dist + 'content/fonts/'))
+            .pipe(rev.manifest(config.revManifest, {
+                base: config.dist,
+                merge: true
+            }))
+            .pipe(gulp.dest(config.dist)),
         gulp.src([config.app + 'robots.txt', config.app + 'favicon.ico', config.app + '.htaccess'], { dot: true })
-        .pipe(plumber({errorHandler: handleErrors}))
-        .pipe(changed(config.dist))
-        .pipe(gulp.dest(config.dist))
+            .pipe(plumber({errorHandler: handleErrors}))
+            .pipe(changed(config.dist))
+            .pipe(gulp.dest(config.dist))
     );
 });
 
